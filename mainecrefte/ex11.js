cbdef3aa15f5650b11a3c402c51e241671271b2b
NicolasBrunatto <brunatto.nicolas@gmail.com>
2024-01-11 12:09:34 -0300

    modified:   mainecrefte/ex11.html 	modified:   mainecrefte/ex11.js 	modified:   mainecrefte/styleEx10.css

--- mainecrefte/ex11.js
+++ mainecrefte/ex11.js
@@ -2,19 +2,71 @@ function main() {
 
     const btnAdd = document.querySelector('#buttonAdd')
     const inputText = document.querySelector('#input')
+    const list = document.querySelector('#ul')
+
+    loadSavedTasks()
 
     btnAdd.addEventListener('click', function(){
         if(inputText.value){
         addInputToList(inputText.value)
         }
+        saveTaks()
+        clearInput()
+        
     })
 
-    function addInputToList(message) {
+    document.addEventListener('click', function(e){
+        const eClicked = e.target
+        if(eClicked.classList.contains('deleteBtn')){
+           eClicked.parentElement.remove()
+        }
+        saveTaks()
+    })
 
-        const list = document.querySelector('#lista')
+    function clearInput(){
+        inputText.value=''
+        inputText.focus()
+    }
+
+    function addInputToList(message) { 
         const li = document.createElement('li')
+        
         li.innerHTML += message
         list.appendChild(li)
+        createDeleteBtn(li)
     }
+
+    function createDeleteBtn(li){
+        const genDelBt = document.createElement('button')
+        genDelBt.classList.add('deleteBtn')
+
+        genDelBt.innerText = 'delete'
+
+        li.appendChild(genDelBt)
+    }
+
+    function saveTaks()
+    {
+        const taskList = ul.querySelectorAll('li')
+        const tasksArray = []
+
+        for (let taskName of taskList) {
+            let taskText = taskName.innerText
+            taskText = taskText.replace('delete', '')
+            taskText = taskText.replace('\n', '')
+            tasksArray.push(taskText)
+        }
+        const tasksJSON = JSON.stringify(tasksArray)
+        localStorage.setItem('tasks', tasksJSON)
+    }
+    
+    function loadSavedTasks(){
+        const tasks = localStorage.getItem('tasks')
+        const taskList = JSON.parse(tasks)
+        for (const i in taskList) {
+            addInputToList(taskList[i])
+        }
+    }
+
 }
-main()
+main() 
