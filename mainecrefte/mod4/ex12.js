c1f1a918d5150c20a0ce75429ed717ca62bd9e3f
NicolasBrunatto <brunatto.nicolas@gmail.com>
2024-01-12 18:32:33 -0300

    modified:   mainecrefte/mod4/ex12.js

--- mainecrefte/mod4/ex12.js
+++ mainecrefte/mod4/ex12.js
@@ -1,19 +1,21 @@
-function rand(min = 1000, max = 3000){
-   const randomNum = Math.random() * (max - min) + min
-   console.log(randomNum)
-   return Math.floor(randomNum)
-}
-
-function f1(callback){
-    if(callback) callback()
-    setTimeout(function(){
-        console.log('f1')
-        
-    },rand())
-}
-
-f1(f1Callback())
-
-function f1Callback(){
-    console.log('aguarde')
-}
+( function(){
+    function rand(min = 1000, max = 3000){
+        const randomNum = Math.random() * (max - min) + min
+        console.log(randomNum)
+        return Math.floor(randomNum)
+     }
+     
+     function f1(callback){
+         if(callback) callback()
+         setTimeout(function(){
+             console.log('f1')
+             
+         },rand())
+     }
+     
+     f1(f1Callback())
+     
+     function f1Callback(){
+         console.log('aguarde')
+     }
+}())
