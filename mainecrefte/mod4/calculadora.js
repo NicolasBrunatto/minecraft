65438ee6364728c204dbc067dd2e29bcd0851742
NicolasBrunatto <brunatto.nicolas@gmail.com>
2024-01-17 16:12:55 -0300

    modified:   mainecrefte/mod4/calculadora.html 	modified:   mainecrefte/mod4/calculadora.js

--- mainecrefte/mod4/calculadora.js
+++ mainecrefte/mod4/calculadora.js
@@ -1,21 +1,5 @@
 (function () {
     const display = document.querySelector('#display')
-    const btnOpen = document.querySelector('#btnOpen')
-    const btnClose = document.querySelector('#btnClose')
-    const btnDot = document.querySelector('#btnDot')
-    const btnMinus = document.querySelector('#btnMinus')
-    const btnDivision = document.querySelector('#btnDivision')
-    const btnMultiply = document.querySelector('#btnMultiply')
-    const btnPlus = document.querySelector('#btnPlus')
-    const btnEnter = document.querySelector('#btnEnter')
-    const btnClear = document.querySelector('#btnAC')
-    const btnBackSpace = document.querySelector('#btnBKSP')
-
-
-    let operator = 0
-    let num1, num2, afterDot 
-    let tamNum1
-
     //writes on the display
     document.addEventListener('click', function (e) {
         if (display.innerHTML.length < 8) {
@@ -23,69 +7,41 @@
                 display.innerText += e.target.innerText
             }
         }
+
+        if (e.target.classList.contains('btnBKSP')) {
+            deleteDisplayElement()
+        }
+        if (e.target.classList.contains('btnEnter')) {
+            pressEnter()
+        }
+        if (e.target.classList.contains('btnAC')) {
+            deleteAll()
+        }
     })
 
     //deletes display elements
-    btnBackSpace.addEventListener('click', function deleteDisplayElement() {
+    function deleteDisplayElement() {
         display.innerHTML = display.innerHTML.slice(0, -1)
-    })
-    //deletes all
-    btnClear.addEventListener('click', function deleteDisplayElement() {
-        display.innerHTML = ''
-    })
-    //dotted numbers
-    btnDot.addEventListener('click', function(){
-
-    })
-    
-
-    //gets operators
-    btnDivision.addEventListener('click', function () {
-        num1 = Number(display.innerHTML)
-        display.innerHTML += '/'
-        tamNum1 = display.innerHTML.length
-        operator = 0        
-    })
-    btnMultiply.addEventListener('click', function () {
-        num1 = Number(display.innerHTML)
-        display.innerHTML += '*'
-        tamNum1 = display.innerHTML.length
-        operator = 1
-    })
-    btnPlus.addEventListener('click', function () {
-        num1 = Number(display.innerHTML)
-        display.innerHTML += '+'
-        tamNum1 = display.innerHTML.length
-        operator = 2
-    })
-    btnMinus.addEventListener('click', function () {
-        num1 = Number(display.innerHTML)
-        display.innerHTML += '-'
-        tamNum1 = display.innerHTML.length
-        operator = 3
-    })
-    //press enter
-    btnEnter.addEventListener('click', function () {
-        num2 = display.innerHTML
-        num2 = Number(num2.slice(tamNum1,display.innerHTML.length))
-        let result = calculate(operator, num1, num2) 
-        let resultString = result.toString()   
-        if(resultString.length > 8){ result = result.toFixed(6)}
-        display.innerHTML = result
-    })
-    //calculates the result
-    function calculate(operator, num1, num2) {
-        if (operator === 0) {
-            return (num1 / num2.toFixed(4))
-        }
-        if (operator === 1) {
-            return (num1 * num2.toFixed(4))
-        }
-        if (operator === 2) {
-            return (num1 + num2.toFixed(4))
-        }
-        if (operator === 3) {
-            return (num1 - num2.toFixed(4))
-        }
     }
+    //deletes all
+    function deleteAll() {
+        display.innerHTML = ''
+    }
+    //press enter
+    function pressEnter() {
+        calc2()
+    }
+
+    function calc2() {
+        let result = (display.innerHTML)
+        result = eval(result)
+        if (result) {
+  
+            let resultString = result.toString()
+            if (resultString.length > 8) { result = result.toFixed(6) }
+            display.innerHTML = result
+        }
+        else { alert('invalid') }
+    }
+
 }())
