db36f19595d5303850f130b0b86c8fe985f59bd2
NicolasBrunatto <brunatto.nicolas@gmail.com>
2024-01-22 10:10:48 -0300

    deleted:    THE MATRIX/canvasTest.html 	modified:   THE MATRIX/matrixcalcSHAPE.html 	modified:   THE MATRIX/matrixcalculatore.js 	modified:   THE MATRIX/style.css 	new file:   mainecrefte/B/2.js

--- THE MATRIX/matrixcalculatore.js
+++ THE MATRIX/matrixcalculatore.js
@@ -1,8 +1,10 @@
 (function () {
     let width1 = 3
     let height1 = 3
-    criaTabela(width1, height1)
-
+    let width2 = 3
+    let height2 = 3
+    criaTabela(height1, width1)
+    criaTabela2(height2, width2)
 
     function criaTabela(height, width) {
         let html = '<table>'
@@ -10,8 +12,30 @@
             html += '<tr>'
             for (let collumn = 0; collumn < width; collumn++) {
 
+                let index = collumn + width * row
+
                 html += '<td>'
-                html += '<input type="number">'
+                html += `<input id='${index}' type="number">`
+                html += '</td>'
+            }
+
+            html += '</tr>'
+
+        }
+        html += '</table>'
+
+        document.querySelector('#matrix1').innerHTML = html
+    }
+    //TABLES 1 and 2
+    function criaTabela2(height, width) {
+        let html = '<table>'
+        for (let row = 0; row < height; row++) {
+            html += '<tr>'
+            for (let collumn = 0; collumn < width; collumn++) {
+                let index = collumn + width * row
+
+                html += '<td>'
+                html += `<input id='${index}' type="number">`
                 html += '</td>'
             }
 
@@ -19,26 +43,114 @@
 
         }
         html += '<tr>'
-        html += `<td colspan="${width}"><button>DET</button>`
+        html += `<td><button ><</button><td><button class="decrease">^</button><td><button class="AC2">AC</button>`
         html += '</tr>'
+
         html += '<tr>'
-        html += `<td colspan="${width}"><button class="increaseWidth" class="btn2">></button><button class="increaseHeight" class="btn2">V</button>`
+        html += `<td colspan="${width}"><button class="increaseWidth2 btn2">></button><button class="increaseHeight2 btn2">V</button>`
+        html += '</tr>'
+
+        html += '<tr>'
+        html += `<td colspan="${width}"><button>DET</button>`
         html += '</tr>'
 
         html += '</table>'
 
-        document.querySelector('#matrix1').innerHTML = html
+        document.querySelector('#matrix2').innerHTML = html
     }
 
-    document.addEventListener('click',function(e){
+    document.addEventListener('click', function (e) {
         const target = e.target
-        if(target.classList.contains('increaseWidth')){
-            increasewidth()
+        //increases
+        if (target.classList.contains('increaseWidth2')) {
+            increaseWidth2()
+            return
         }
+        if (target.classList.contains('increaseHeight2')) {
+            increaseHeight2()
+            return
+        }
+        if (target.classList.contains('increaseWidth')) {
+            increaseWidth()
+            return
+        }
+        if (target.classList.contains('increaseHeight')) {
+            increaseHeight()
+            return
+        }
+        //decreases
+        if (target.classList.contains('decreaseWidth2')) {
+            decreaseWidth2()
+            return
+        }
+        if (target.classList.contains('decreaseHeight2')) {
+            decreaseHeight2()
+            return
+        }
+        if (target.classList.contains('decreaseWidth')) {
+            decreaseWidth()
+            return
+        }
+        if (target.classList.contains('decreaseHeight')) {
+            decreaseHeight()
+            return
+        }
+        //clear
+        if (target.classList.contains('AC1')) {
+            clearImputs1()
+            return
+        }
+        if (target.classList.contains('AC2')) {
+            clearImputs2()
+            return
+        }
+
     })
+    //increase
+    function increaseWidth() {
+        width1++
+        criaTabela(height1, width1)
+    }
 
-    function increasewidth(){
+    function increaseHeight() {
+        height1++
+        criaTabela(height1, width1)
+    }
+    function increaseWidth2() {
+        width2++
+        criaTabela2(height2, width2)
+    }
+    function increaseHeight2() {
+        height2++
+        criaTabela2(height2, width2)
+    }
 
+    //decrease
+    function decreaseWidth() {  
+        if(width1>1) width1--
+         criaTabela(height1, width1)
+        
+
+    }
+
+    function decreaseHeight() {
+        if(height1>1) height1--
+        criaTabela(height1, width1)
+    }
+    function decreaseWidth2() {
+        if(width1>1) width2--
+        criaTabela2(height2, width2)
+    }
+    function decreaseHeight2() {
+        if(height2>1)height2--
+        criaTabela2(height2, width2)
+    }
+
+    function clearImputs1() {
+     criaTabela(height1,width1)
+    }
+    function clearImputs2() {
+        criaTabela2(height2,width2)
     }
 
 }())
